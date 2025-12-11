4a55984e91d472b1d8f931f473590bcb91f00930
Sven Hoehne <sven.hoehne@hotmail.com>
2025-12-11 10:12:43 +0100

    Update 2025-12-11

--- link-linux.sh
+++ link-linux.sh
@@ -1,25 +1,31 @@
 zen_profile=
 
-rm -rfv ($env.HOME)/.config/nvim
-rm -rfv ($env.HOME)/.config/waybar
-rm -rfv ($env.HOME)/.config/wezterm
-rm -rfv ($env.HOME)/.config/hypr
-rm -rfv ($env.HOME)/.config/background_images
-rm -rfv ($env.HOME)/.config/nushell
-rm -rfv ($env.HOME)/.config/starship.toml
-rm -fv ($env.HOME)/.config/.ignore
-rm -rfv ($env.HOME)/.config/fuzzel
-rm -rfv ($env.HOME)/.config/uwsm
-rm -rfv "($env.HOME)/.zen/$zen_profile.Default (release)"
+rm -rfv $HOME/.config/nvim
+rm -rfv $HOME/.config/waybar
+rm -rfv $HOME/.config/wezterm
+rm -rfv $HOME/.config/hypr
+rm -rfv $HOME/.config/background_images
+rm -rfv $HOME/.config/nushell
+rm -rfv $HOME/.config/starship.toml
+rm -fv $HOME/.config/.ignore
+rm -rfv $HOME/.config/fuzzel
+rm -rfv $HOME/.config/uwsm
+rm -rfv "$HOME/.zen/$zen_profile.Default (release)/chrome"
+rm -fv "$HOME/.zen/$zen_profile.Default (release)/zen-keyboard-shortcuts.json"
+rm -fv "$HOME/.zen/$zen_profile.Default (release)/zen-themes.json"
+rm -fv "$HOME/.zen/$zen_profile.Default (release)/prefs.js"
 
-ln -s ($env.PWD)/nvim ($env.HOME)/.config/nvim
-ln -s ($env.PWD)/waybar ($env.HOME)/.config/waybar
-ln -s ($env.PWD)/wezterm ($env.HOME)/.config/wezterm
-ln -s ($env.PWD)/hypr ($env.HOME)/.config/hypr
-ln -s ($env.PWD)/background_images ($env.HOME)/.config/background_images
-ln -s ($env.PWD)/nushell ($env.HOME)/.config/nushell
-ln -s ($env.PWD)/starship/starship.toml ($env.HOME)/.config/starship.toml
-ln -s ($env.PWD)/.ignore ($env.HOME)/.config/.ignore
-ln -s ($env.PWD)/fuzzel ($env.HOME)/.config/fuzzel
-ln -s ($env.PWD)/uwsm ($env.HOME)/.config/uwsm
-ln -s ($env.PWD)/zen-profile "($env.HOME)/.zen/$zen_profile.Default (release)"
+ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/waybar $HOME/.config/waybar
+ln -s $PWD/wezterm $HOME/.config/wezterm
+ln -s $PWD/hypr $HOME/.config/hypr
+ln -s $PWD/background_images $HOME/.config/background_images
+ln -s $PWD/nushell $HOME/.config/nushell
+ln -s $PWD/starship/starship.toml $HOME/.config/starship.toml
+ln -s $PWD/.ignore $HOME/.config/.ignore
+ln -s $PWD/fuzzel $HOME/.config/fuzzel
+ln -s $PWD/uwsm $HOME/.config/uwsm
+ln -s $PWD/zen-profile/chrome "$HOME/.zen/$zen_profile.Default (release)/chrome"
+ln -s $PWD/zen-profile/zen-keyboard-shortcuts.json "$HOME/.zen/$zen_profile.Default (release)/zen-keyboard-shortcuts.json"
+ln -s $PWD/zen-profile/zen-themes.json "$HOME/.zen/$zen_profile.Default (release)/zen-themes.json"
+ln -s $PWD/zen-profile/prefs.js "$HOME/.zen/$zen_profile.Default (release)/prefs.js"
